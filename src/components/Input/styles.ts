fba5620bbe66b53298487764c6dec440b53c4214
Paula Lopes <pls21@msn.com>
2021-04-10 14:46:06 -0300

    refactor: change Input js to ts

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,6 +1,7 @@
 import styled, { css } from 'styled-components';
+interface ContainerProps { isFocused: boolean; isFilled: boolean; }
 
-export const Container = styled.div`
+export const Container = styled.div<ContainerProps>`
   display: flex;
   align-items: center;
 
