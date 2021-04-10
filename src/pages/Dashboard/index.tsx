c986f73b344d77c6b75de7c02505f3167a8be85a
Paula Lopes <pls21@msn.com>
2021-04-10 14:25:04 -0300

    refactor: change Head js to ts

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Food } from '../../components/Food';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
 import ModalAddFood from '../../components/ModalAddFood';
 import ModalEditFood from '../../components/ModalEditFood';
 import api from '../../services/api';
